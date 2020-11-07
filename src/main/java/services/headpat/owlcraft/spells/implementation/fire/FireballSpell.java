001ce949176fcc99aba59cea09448c296e6eb960
Sun <gabehttps@gmail.com>
2020-11-07 13:21:18 -0800

    fix Fireball spell

--- src/main/java/services/headpat/owlcraft/spells/implementation/fire/FireballSpell.java
+++ src/main/java/services/headpat/owlcraft/spells/implementation/fire/FireballSpell.java
@@ -20,7 +20,6 @@ import services.headpat.owlcraft.spells.SpellContext;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class FireballSpell extends Spell {
 	private final ShapelessRecipe glyphRecipe;
@@ -53,15 +52,11 @@ public class FireballSpell extends Spell {
 		}
 		((LivingEntity) entity).addPotionEffect(new PotionEffect(PotionEffectType.GLOWING, 10, 1, false, false, false));
 
-
 		Location location = entity.getLocation().add(0, 1.5, 0);
 		Vector direction = entity.getLocation().getDirection().normalize();
-		AtomicReference<Location> lastLocation = new AtomicReference<>();
 
 		MutableInt iteration = new MutableInt(0);
 		BukkitTask task = Bukkit.getScheduler().runTaskTimer(OwlCraft.getInstance(), () -> {
-			lastLocation.set(location.clone());
-
 			iteration.add(1);
 			double d = (iteration.toInteger() - 1) * 0.5;
 
@@ -77,21 +72,28 @@ public class FireballSpell extends Spell {
 
 			entity.getWorld().spawnParticle(Particle.REDSTONE, location, 10, 0.0, 0.0, 0.0, 1, new Particle.DustOptions(Color.RED, 2));
 			entity.getWorld().spawnParticle(Particle.CRIT_MAGIC, location, 8, 0.0, 0.0, 0.0, 1);
-			double d2 = iteration.toInteger() * 0.5;
-			double x2 = direction.getX() * (d2 * d2);
-			double y2 = direction.getY() * (d2 * d2);
-			double z2 = direction.getZ() * (d2 * d2);
-			Location loc = location.clone().add(x2, y2, z2);
-			if (!loc.getBlock().isEmpty() || iteration.toInteger() > 9) {
-				lastLocation.get().getWorld().createExplosion(lastLocation.get(), 4f, true, false);
+
+			if (!location.getBlock().isPassable()) {
+				entity.getWorld().createExplosion(location, 5f, true, true, entity);
+				if (glyphStack != null)
+					glyphStack.setAmount(glyphStack.getAmount() - 1);
 				this.getSpellManager().setInactive(this, entity);
-				glyphStack.setAmount(glyphStack.getAmount() - 1);
-				return;
+			} else {
+				entity.getWorld().getNearbyEntities(location, 1.0, 1.0, 1.0).forEach((target) -> {
+					if (target.equals(entity) || !(target instanceof LivingEntity) || !(this.isTargetable(entity, target))) {
+						return;
+					}
+
+					target.getWorld().createExplosion(target.getLocation(), 5f, true, true, entity);
+					if (glyphStack != null)
+						glyphStack.setAmount(glyphStack.getAmount() - 1);
+					this.getSpellManager().setInactive(this, entity);
+				});
 			}
 
 			location.subtract(x, y, z);
 		}, 0, 2);
 		this.getSpellManager().setActive(this, entity, new SpellContext<>(task::cancel));
-		return false;
+		return glyphStack == null;
 	}
 }
