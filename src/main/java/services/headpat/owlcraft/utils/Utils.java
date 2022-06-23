63261bb0b43ab9b1f1e5bf5f693e5e685a559bb3
gabehxd <gabehttps@gmail.com>
2022-06-23 14:31:24 -0700

    clean up extra code

--- src/main/java/services/headpat/owlcraft/utils/Utils.java
+++ src/main/java/services/headpat/owlcraft/utils/Utils.java
@@ -1,17 +1,11 @@
 package services.headpat.owlcraft.utils;
 
 import lombok.experimental.UtilityClass;
-import net.kyori.adventure.text.Component;
-import org.bukkit.enchantments.Enchantment;
-import org.bukkit.inventory.ItemFlag;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.potion.PotionEffectType;
-import services.headpat.spigotextensions.utils.ChatUtils;
 
 @UtilityClass
 public class Utils {
-    public static final PotionEffectType[] POSITIVE_EFFECTS = {
+    public static final PotionEffectType[] POSITIVE_POTION_EFFECTS = {
             PotionEffectType.ABSORPTION,
             PotionEffectType.DAMAGE_RESISTANCE,
             PotionEffectType.DOLPHINS_GRACE,
@@ -29,21 +23,4 @@ public class Utils {
             PotionEffectType.SLOW_FALLING,
             PotionEffectType.SPEED,
     };
-
-    public static void addBaseFlags(ItemMeta meta) {
-        meta.addEnchant(Enchantment.DURABILITY, 5, true);
-        meta.addItemFlags(ItemFlag.HIDE_ENCHANTS, ItemFlag.HIDE_ATTRIBUTES);
-    }
-
-    public static ItemStack appendNameAndLore(ItemStack stack, String name, String lore) {
-        if (stack.hasItemMeta()) {
-            ItemMeta meta = stack.getItemMeta();
-            if (!name.equals(""))
-                meta.displayName(Component.text(name));
-            if (!lore.equals(""))
-                meta.lore(ChatUtils.createLore(lore));
-            stack.setItemMeta(meta);
-        }
-        return stack;
-    }
 }
